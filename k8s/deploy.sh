c02a147d22a02813ce2c7f4354e3b7f9f7ee7df3
Marvin <geitnermarvin@gmail.com>
2025-03-21 19:55:00 +0100

    Added app and ressources

---
+++ k8s/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME=k8s
+IMAGE_TAG=latest
+REGISTRY=192.168.178.29:5000
+
+echo "Building Docker image: $REGISTRY/$IMAGE_NAME:$IMAGE_TAG..."
+docker build -t "$REGISTRY/$IMAGE_NAME:$IMAGE_TAG" .
+
+echo "Pushing Docker image to $REGISTRY..."
+docker push "$REGISTRY/$IMAGE_NAME:$IMAGE_TAG"
+
+echo "Docker image $REGISTRY/$IMAGE_NAME:$IMAGE_TAG successfully pushed!"
